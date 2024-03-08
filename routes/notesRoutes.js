91398cb268986a3c6998f2c39f1d2faf0c93332e
Michael Chiem <michaelchiem1@hotmail.com>
2024-03-08 12:27:02 -0700

    Updating the route files

---
+++ routes/notesRoutes.js
@@ -0,0 +1,58 @@
+// routes/notesRoutes.js
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const fs = require('fs');
+
+const dbFilePath = path.join(__dirname, '../db/db.json'); // Adjust the path here
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('/api/notes', (req, res) => {
+    const jsonData = fs.readFileSync(dbFilePath, 'utf8');
+    const notes = JSON.parse(jsonData);
+    res.json(notes);
+});
+
+router.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text,
+        id: Math.floor(Math.random() * 10000),
+    }
+
+    const jsonData = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8');
+
+    const notes = JSON.parse(jsonData);
+
+    notes.push(newNote);
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+
+    res.json(newNote);
+});
+
+router.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    console.log(noteId)
+
+
+    const jsonData = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8');
+    let notes = JSON.parse(jsonData);
+    notes = notes.filter(n => n.id != noteId);
+
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+
+    res.json("successfully deleted note");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-﻿
+﻿// server.js
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3001;
+
+const notesRoutes = require('./routes/notesRoutes');
+const indexRoutes = require('./routes/indexRoutes');
+
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(publicPath));
+
+app.use('/', indexRoutes);
+app.use('/', notesRoutes);
+
+app.listen(PORT, () =>
+    console.log(`Listening for requests on port ${PORT}! 🏎️`)
+);
